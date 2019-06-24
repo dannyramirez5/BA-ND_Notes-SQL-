2b17a301ccf6e34360f448c2026ff9a29e6403ac
Danny Ramirez <danielramirez546@gmail.com>
2019-06-23 22:23:26 -0400

    Update Subquery practice.sql

--- Subquery practice.sql
+++ Subquery practice.sql
@@ -1,4 +1,9 @@
-SELECT t3.id, t3.name, t3.channel, t3.ct
+/*=====================================================================================
+Just simple subquery practice statements using a Udacity db.
+=====================================================================================*/
+
+
+SELECT t3.id, t3.name, t3.channel, t3.ct
 
 	/* Here I retrieve the count of instances of each channel for each name/id */
   FROM (SELECT a.id, a.name, we.channel, COUNT(*) ct
@@ -6,6 +11,7 @@
 	JOIN web_events we
 	ON a.id = we.account_id
 	GROUP BY a.id, a.name, we.channel) t3
+	
 	/*This JOIN retrieves a list of MAX counts for each if which I will then match up to the original list with all the counts and only pick out the entries that match the id and MAX count.*/
   JOIN (SELECT t1.id, t1.name, MAX(ct) max_chan
 	FROM (SELECT a.id, a.name, we.channel, COUNT(*) ct
